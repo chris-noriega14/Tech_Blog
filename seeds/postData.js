33f27560888e8868f5dcc01f5765a62826b80ff5
chris-noriega14 <chris.noriega14@hotmail.com>
2021-11-27 00:28:11 -0600

    Added functionality to add posts on the user's profile page

--- seeds/postData.js
+++ seeds/postData.js
@@ -3,31 +3,31 @@ const { Post } = require('../models');
 const postData = [
   {
     title: 'Plain Shorts',
-    contents: 24.99,
+    contents: "Testing!",
     username: "Test1",
     user_id: 1,
   },
   {
     title: 'Plain T-Shirt',
-    contents: 34.99,
+    contents: "Testing!",
     username: "Test2",
     user_id: 2,
   },
   {
     title: 'Plain Pants',
-    contents: 44.99,
+    contents: "Testing!",
     username: "Test2",
     user_id: 2,
   },
   {
     title: 'Plain Shoes',
-    contents: 54.99,
+    contents: "Testing!",
     username: "Test1",
     user_id: 1,
   },
   {
     title: 'Plain Hat',
-    contents: 64.99,
+    contents: "Testing!",
     username: "Test3",
     user_id: 3,
   },
